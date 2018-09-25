11b4122f044bf1f6a6540a4b40b25729c3da022f
Viktor Kratz <viktor.kratz@mni.thm.de>
2018-09-25 16:58:21 +0200

    Combobox to select versions is only enabled if if chprevious checkbox in the form is checked. The code is using java script blocks in the middle of the body - it's a bit dirty an may be changed later.

--- pp_search_gene_version_input.php
+++ pp_search_gene_version_input.php
@@ -1,10 +1,22 @@
 <?php
 	$dbconn = pg_connect("host=localhost dbname=pp_annot3 user=web_usr password=moss33!")
 	or die('Could not connect: ' . pg_last_error());
-	$query='select distinct genome_version from "public"."gene";';
+	$query='select distinct genome_version from "public"."gene" order by genome_version asc;';
 	$versions_res=pg_query($query) or die('Cannot access list of genome versions: ' . pg_last_error());
 ?>
-<select name="selGeneVersion">
+<input type="checkbox" id="chkGeneSearch">Search for a specific gene version</input>
+<!-- Script to activate and deactivate combo box if checkbox is checked or unchecked. This code block includes the jquery file contained in js - this is a bit dirty because it may be included multiple times if other php files are doing the same. It's also possible to write this function in to a seperate javascript file - this would be less dirty but it's not possible to seperate this part of a form in a seperate php file in this case. -->
+<script src="js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript">
+$('#chkGeneSearch').change(function(){
+	if($(this).is(":checked"))
+		$('#selGeneVersion').prop('disabled',false);
+	else
+		$('#selGeneVersion').prop('disabled',true);
+	});
+</script>
+
+<select id="selGeneVersion" disabled="true">
 	<?php
 		$versions=pg_fetch_all_columns($versions_res) or die("Invalid result after version-request:".pg_last_error());
 		foreach($versions as $version)

63255482bf98c589c3118674b95e287bb8d21136
Viktor Kratz <viktor.kratz@mni.thm.de>
2018-09-26 13:21:29 +0200

    Added comment and closing connection

--- pp_search_gene_version_input.php
+++ pp_search_gene_version_input.php
@@ -1,10 +1,12 @@
 <?php
+	 // Connecting to database and retrival of all Versions.
 	 include_once 'pp_database_data.php';
 	$dbconn = pg_connect($connectionString)
 	or die('Could not connect: ' . pg_last_error());
 	$query='select distinct genome_version from "public"."gene" order by genome_version asc;';
 	$versions_res=pg_query($query) or die('Cannot access list of genome versions: ' . pg_last_error());
 ?>
+
 <input type="checkbox" id="chkGeneSearch">Search for a specific gene version</input>
 <!-- Script to activate and deactivate combo box if checkbox is checked or unchecked. This code block includes the jquery file contained in js - this is a bit dirty because it may be included multiple times if other php files are doing the same. It's also possible to write this function in to a seperate javascript file - this would be less dirty but it's not possible to seperate this part of a form in a seperate php file in this case. -->
 <script src="js/jquery-3.1.1.min.js"></script>
@@ -26,4 +28,7 @@ $('#chkGeneSearch').change(function(){
 		}
 	?>
 	</select
-	
+	<?php
+	// Closing connection
+pg_close($dbconn);
+?>

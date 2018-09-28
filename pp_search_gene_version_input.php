922a29070c28973e356362cc9b68fc665e1b2f36
Viktor Kratz <viktor.kratz@mni.thm.de>
2018-09-28 14:38:28 +0200

    Changing form-control to form-group, adding checkbox which can be checked to turn the phonetic search off

--- pp_search_gene_version_input.php
+++ pp_search_gene_version_input.php
@@ -6,7 +6,7 @@
 	$query='select distinct genome_version from "public"."gene" order by genome_version asc;';
 	$versions_res=pg_query($query) or die('Cannot access list of genome versions: ' . pg_last_error());
 ?>
-<div class="form-control">
+<div class="form-group">
 	<input type="checkbox" id="chkGeneSearch" class="checkbox">Search for a specific gene version</input>
 	<!-- Script to activate and deactivate combo box if checkbox is checked or unchecked. This code block includes the jquery file contained in js - this is a bit dirty because it may be included multiple times if other php files are doing the same. It's also possible to write this function in to a seperate javascript file - this would be less dirty but it's not possible to seperate this part of a form in a seperate php file in this case. -->
 	<script type="text/javascript">

b2cbb622e30ee11e1e72b1139ac632f1a4b3deaa
Viktor Kratz <viktor.kratz@mni.thm.de>
2018-09-28 14:12:06 +0200

    Added bootstrap classes to checkboxes

--- pp_search_gene_version_input.php
+++ pp_search_gene_version_input.php
@@ -7,7 +7,7 @@
 	$versions_res=pg_query($query) or die('Cannot access list of genome versions: ' . pg_last_error());
 ?>
 
-<input type="checkbox" id="chkGeneSearch">Search for a specific gene version</input>
+<input type="checkbox" id="chkGeneSearch" class="checkbox">Search for a specific gene version</input>
 <!-- Script to activate and deactivate combo box if checkbox is checked or unchecked. This code block includes the jquery file contained in js - this is a bit dirty because it may be included multiple times if other php files are doing the same. It's also possible to write this function in to a seperate javascript file - this would be less dirty but it's not possible to seperate this part of a form in a seperate php file in this case. -->
 <script src="js/jquery-3.1.1.min.js"></script>
 <script type="text/javascript">
@@ -18,7 +18,7 @@ $('#chkGeneSearch').change(function(){
 		$('[name=selGeneVersion]').prop('disabled',true);
 	});
 </script>
-
+<div class="checkbox">
 	<?php
 		$versions=pg_fetch_all_columns($versions_res) or die("Invalid result after version-request:".pg_last_error());
 		foreach($versions as $version)
@@ -26,6 +26,7 @@ $('#chkGeneSearch').change(function(){
 			echo "<input type='checkbox' disabled='true' name='selGeneVersion'>" . $version . "</input>";
 		}
 	?>
+</div>
 	<?php
 	// Closing connection
 pg_close($dbconn);

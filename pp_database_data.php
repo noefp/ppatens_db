363a8e8432642b34f4238481bb731ef59053e461
Viktor Kratz <viktor.kratz@mni.thm.de>
2018-10-01 15:13:02 +0200

    Adding function which returns query to retrieve all versions currently existing in the database, added function which returns a query to get all gene versions associated with the search keyword.

--- pp_database_data.php
+++ pp_database_data.php
@@ -1,3 +1,25 @@
 <?php
 $connectionString="host=localhost dbname=pp_annot3 user=web_usr password=moss33!";
+function getGenesForSearchAndVersion($searchString, $versions)
+{
+	$searchString=pg_escape_string($searchString); 
+	$versions=implode(",",array_map(function ($versionString) { return "'" . pg_escape_string($versionString) ."'"; },$versions));
+	 	return "select array_agg((gene_id, genome_version, gene_name)) as geneRow
+		from gene inner join 
+		(SELECT array_agg(distinct gene_id1) || array_agg(distinct gene_id2) 
+	 as gene_ids
+	 FROM gene ge1
+	 right join gene_gene 
+	 on ge1.gene_id=gene_id1 or ge1.gene_id=gene_id2
+WHERE ge1.gene_name ILIKE '%" . $searchString . "%' 
+	 group by gene_id1) as geneArrays
+ on gene_id = any(gene_ids)
+ where genome_version in(" . $versions . ")
+ group by gene_ids;";
+
+}
+function getVersionsQuery()
+{
+	return 'select distinct genome_version from "public"."gene" order by genome_version asc;';
+}
 ?>

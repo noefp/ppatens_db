c39aa2b276030b8eec512d013cfe26a7b64e7f52
Viktor Kratz <viktor.kratz@mni.thm.de>
2018-10-01 15:14:44 +0200

    Changed form so it's possible to get more than one version wusing php

--- pp_search_gene_version_input.php
+++ pp_search_gene_version_input.php
@@ -3,19 +3,19 @@
 	 include_once 'pp_database_data.php';
 	$dbconn = pg_connect($connectionString)
 	or die('Could not connect: ' . pg_last_error());
-	$query='select distinct genome_version from "public"."gene" order by genome_version asc;';
+	$query=getVersionsQuery();
 	$versions_res=pg_query($query) or die('Cannot access list of genome versions: ' . pg_last_error());
 ?>
 <div class="form-group">
-	<input type="checkbox" id="chkGeneSearch" class="checkbox">Search for a specific gene version</input>
+	<input type="checkbox" id="chkGeneSearch" class="checkbox" name="version_search">Search for a specific gene version</input>
 	<!-- Script to activate and deactivate combo box if checkbox is checked or unchecked. This code block includes the jquery file contained in js - this is a bit dirty because it may be included multiple times if other php files are doing the same. It's also possible to write this function in to a seperate javascript file - this would be less dirty but it's not possible to seperate this part of a form in a seperate php file in this case. -->
 	<script type="text/javascript">
 		$('#chkGeneSearch').change(function()
 		{
 			if($(this).is(":checked"))
-				$('[name=selGeneVersion]').prop('disabled',false);
+				$('[name="selGeneVersion[]"]').prop('disabled',false);
 			else
-				$('[name=selGeneVersion]').prop('disabled',true);
+				$('[name="selGeneVersion[]"]').prop('disabled',true);
 		});
 	</script>
 	<div class="checkbox">
@@ -23,7 +23,7 @@
 			$versions=pg_fetch_all_columns($versions_res) or die("Invalid result after version-request:".pg_last_error());
 			foreach($versions as $version)
 			{
-				echo "<input type='checkbox' disabled='true' name='selGeneVersion'>" . $version . "</input>";
+				echo "<input type='checkbox' disabled='true' name='selGeneVersion[]' value='" . $version ."'>" . $version . "</input>";
 			}
 		?>
 	</div>

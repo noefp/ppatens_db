b878341c4534fbc85f403ea50d3e20664dfdefbb
Viktor Kratz <viktor.kratz@mni.thm.de>
2018-10-16 12:13:02 +0200

    Creating a table containing gene_name and version of compared genes.

--- pp_compare_results_view.php
+++ pp_compare_results_view.php
@@ -6,8 +6,12 @@ $gNameValues=implode(",",array_map(function($input) { return trim(pg_escape_stri
 $query="SELECT gene_id, searchValues.gene_name, genome_version FROM gene right join unnest('{{$gNameValues}}'::text[]) WITH ORDINALITY AS searchValues(gene_name,ord) using(gene_name) order by searchValues.ord asc";
 $dbconn = pg_connect($connectionString)
 	or die('Could not connect: ' . pg_last_error());
-$dbRes=pg_query($query);
-
+$dbRes=pg_query($query) or die('Query failed: ' . pg_last_error());
+echo "<table><thead><tr><th>Gene name</th><th>Version</th></tr></thead><tbody>";
+while($row=pg_fetch_array($dbRes,null, PGSQL_ASSOC)) {
+	echo "<tr><td><a href=\"pp_annot.php?name={$row["gene_name"]}\">{$row["gene_name"]}</a></td><td>{$row["genome_version"]}</td></tr>";
+}	
+echo "</tbody></table>";
 // Freeing result and closing connection.
 pg_free_result($dbRes);
 pg_close($dbconn);

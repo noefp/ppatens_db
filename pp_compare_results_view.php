31430eda197bbefcd01240c32b150b35599bcff4
Viktor Kratz <viktor.kratz@mni.thm.de>
2018-10-24 15:02:39 +0200

    Different versions are shown in multiple rows in compare view. Empty lines are ignored better now.

--- pp_compare_results_view.php
+++ pp_compare_results_view.php
@@ -1,7 +1,9 @@
 <?php
 include_once "ppdb_header.php";
 include "pp_database_data.php";
-if(empty($_GET["txtGenes"]))
+$gNamesArr=array_filter(explode("\n",$_GET["txtGenes"]),function($gName) {return ! empty($gName);});
+
+if(sizeof($gNamesArr)==0)
 {
 	echo "<h1>No genes to search provided.</h1>";
 }
@@ -15,13 +17,14 @@ else
 	$query="SELECT distinct annot_type from annotation order by annot_type";
 	$res=pg_query($query) or die("Couldn't query database.");
 	$annotTypes=pg_fetch_all_columns($res);
-	$gNamesArr=explode("\n",$_GET["txtGenes"]);
 	$gNameValues=implode(",",array_map(function($input) {if(empty(trim($input))) return ""; else  return "'" . trim(pg_escape_string($input))."'" ;},$gNamesArr));
-	 $query="SELECT gene.gene_id, searchValues.gene_name, genome_version, array_agg((annotation.annot_term, annotation.annot_type)) \"annot\"
-	FROM (gene inner join gene_annotation on gene_annotation.gene_id=gene.gene_id) 
+	 $query="SELECT g.gene_id, g.gene_name, g.genome_version, array_agg((annotation.annot_term, annotation.annot_type)) \"annot\"
+	FROM 
+	gene g inner join gene_gene on gene_id1=g.gene_id or gene_id2=g.gene_id
+	inner join (gene g2 inner join gene_annotation on gene_annotation.gene_id=g2.gene_id) on g2.gene_id=gene_id1 or g2.gene_id=gene_id2
 	inner join annotation on annotation.annotation_id=gene_annotation.annotation_id
-	right join unnest(array[{$gNameValues}]) WITH ORDINALITY AS searchValues(gene_name,ord) using(gene_name) 
-	group by gene.gene_id, searchValues.gene_name, searchValues.ord, genome_version 
+	right join unnest(array[{$gNameValues}]) WITH ORDINALITY AS searchValues(search_name,ord) on search_name=g2.gene_name
+	group by g.gene_id, g.gene_name, searchValues.ord, g.genome_version 
 	order by searchValues.ord asc";
 	$dbRes=pg_query($query) or die('Query failed: ' . pg_last_error());
 	echo "<table class=\"table\" id=\"tblResults\"><thead><tr><th>Gene name</th><th>Version</th>";

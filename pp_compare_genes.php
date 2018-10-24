448e62fc5c159b239f7183441b73bd4367b439e0
Viktor Kratz <viktor.kratz@mni.thm.de>
2018-10-24 09:41:54 +0200

    Download fasta file directly in first compare view.

--- pp_compare_genes.php
+++ pp_compare_genes.php
@@ -8,11 +8,21 @@
 			</button>
 		</div>
 		<div class="modal-body">
+			
 			<form method="get" action="pp_compare_results_view.php">
-				<textarea name="txtGenes"></textarea>
+				<textarea name="txtGenes" id="txtGenes"></textarea>
 				<input type="checkbox" name="chkShowAnnot">Show annotations</input>
 				<input type="submit" class="btn btn-search">search</input>
 			</form>
+							<button id="btnDownload">Download as fasta</button>
+
+			<script type="text/javascript">
+				$("#btnDownload").click(function()
+				{
+					
+					window.open("pp_blastdbcmd.php?filename=results.fasta&" + $("#txtGenes").prop("value").split("\n").map(function(row){return "gids[]=" + row;}).join("&"));
+				});
+			</script>
 		</div>
 		<div class="modal-footer">
 		</div>

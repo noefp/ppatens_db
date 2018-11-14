3c206347cd0627a9f5454d599fbca95e6ebaddcd
noefp <noeisneo@gmail.com>
2018-11-14 10:09:30 +0100

    gene lookup style and download names

--- pp_compare_genes.php
+++ pp_compare_genes.php
@@ -9,8 +9,6 @@
 		<div class="modal-body" style="height:355px">
 
 			<form id="gene_version_lookup">
-			<!-- <form method="get" action="pp_compare_results_view.php"> -->
-			<!-- <form id="gene_version_lookup" method="get" action="pp_compare_results_view.php"> -->
         <label for="txtGenes">Paste a list of gene IDs</label>
 				<textarea name="txtGenes" id="txtGenes" class="form-control" rows="10">
 Pp3c1_10000V3.1
@@ -23,8 +21,7 @@ Pp3c1_21730V3.1
 				include_once "pp_search_gene_version_input.php";
 				getCheckboxes("chkVersionName");
 				?>
-				<button type="submit" class="btn btn-success pull-right" form="gene_version_lookup" formaction="pp_compare_results_view.php" formmethod="get" formenctype="multipart/form-data">search</button>
-				<!-- <input type="submit" class="btn btn-search">search</input> -->
+				<button type="submit" class="btn btn-success pull-right" form="gene_version_lookup" formaction="pp_compare_results_view.php" formmethod="get">search</button>
 
 			</form>
 
--- pp_compare_results_view.php
+++ pp_compare_results_view.php
@@ -1,6 +1,12 @@
 <?php
 include_once "ppdb_header.php";
 include "pp_database_data.php";
+?>
+
+<div class="page_container" style="margin-top:40px">
+<div class="data_table_frame">
+
+<?php
 $gNamesArr=array_filter(explode("\n",trim($_GET["txtGenes"])),function($gName) {return ! empty($gName);});
 
 if(sizeof($gNamesArr)==0)
@@ -16,7 +22,7 @@ else
 	{
 		$versions=array_map(function($versionItem) {return trim($versionItem); },$_GET["chkVersionName"]);
 	$versionWhere="where g.genome_version in(" . implode(",",
-	array_map( function($versionItem) 
+	array_map( function($versionItem)
 		{return "'" . pg_escape_string($versionItem) . "'"; },$_GET["chkVersionName"])
 	) . ") or g.gene_name=searchValues.search_name";
 	}
@@ -25,35 +31,35 @@ else
 		$res=pg_query(getVersionsQuery()) or die("Couldn't query database.");
 		$versions=pg_fetch_all_columns($res);
 	}
-	
+
 	// Getting all annotation types.
 	$query="SELECT distinct annot_type from annotation where annot_type not like 'GO %' order by annot_type desc";
 	$res=pg_query($query) or die("Couldn't query database.");
 	$annotTypes=pg_fetch_all_columns($res);
 	$gNameValues=implode(",",array_map(function($input) {if(empty(trim($input))) return ""; else  return "'" . trim(pg_escape_string($input))."'" ;},$gNamesArr));
 	 $query="SELECT searchValues.search_name as \"input\", array_agg( distinct (g.gene_name, g.genome_version)) as \"genes\", array_agg(distinct (annotation.annot_desc, annotation.annot_type)) \"annot\"
-	FROM 
+	FROM
 	gene g inner join gene_gene on gene_id1=g.gene_id or gene_id2=g.gene_id
 	inner join (gene g2 inner join gene_annotation on gene_annotation.gene_id=g2.gene_id) on g2.gene_id=gene_id1 or g2.gene_id=gene_id2
 	inner join annotation on annotation.annotation_id=gene_annotation.annotation_id
 	right join unnest(array[{$gNameValues}]) WITH ORDINALITY AS searchValues(search_name,ord) on search_name=g2.gene_name
 		{$versionWhere}
-	group by searchValues.search_name, searchValues.ord 
+	group by searchValues.search_name, searchValues.ord
 	order by searchValues.ord desc";
 	$dbRes=pg_query($query) or die('Query failed: ' . pg_last_error());
-	echo $query;
+	//echo $query;
 	echo "<table class=\"table\" id=\"tblResults\"><thead><tr><th>input</th>";
 foreach($versions as $versionItem)
 {
 	echo "<th>{$versionItem}</th>";
 }
-	
+
 	if(isset($_GET["chkShowAnnot"])) echo implode("",array_map(function($type) {return "<th>{$type}</th>";},$annotTypes));
 	echo "</tr></thead><tbody>";
 	while($row=pg_fetch_array($dbRes,null, PGSQL_ASSOC)) {
-		
+
 		// Creating Gene columns:
-		
+
 		// Interpreting array returned by database - removing 3 characters in the end and at the beginning.
 		$geneEntries=array_map(function($geneCol) { return explode(",",$geneCol);},explode(")\",\"(",substr($row["genes"],3,-3)));
 		// Removing \" enclosing the the multi word gene names.
@@ -65,34 +71,60 @@ foreach($versions as $versionItem)
 		array_filter($geneEntries,function($item) use($geneVersion) { return $item[1] == $geneVersion;})),
 		";")
 		. "</td>";},$versions));
-		
+
 		// Get all anotations for this row and creating the columns.
 		$annotStr="";
-		if(isset($_GET["chkShowAnnot"])) 
+		if(isset($_GET["chkShowAnnot"]))
 		{
 			// Interpreting array returned by database - removing 3 characters in the end and at the beginning.
 			$annotEntries=array_map(function($annotRow) { return explode(",",$annotRow);},explode(")\",\"(",substr($row["annot"],3,-3)));
 			// Removing \" enclosing the the multi word annotation types.
 			array_walk($annotEntries,function(&$entry){$entry[1]=str_replace("\\\"","",$entry[1]);});
 			// Creating columns for each annotation filled with content if a matching annotation was found in the array.
-			$annotStr=implode(array_map(function($type) use($annotEntries) {return "<td>" . 
+			$annotStr=implode(array_map(function($type) use($annotEntries) {return "<td>" .
 					implode(
 						array_map(function($currAnnot){return str_replace("\\\"","",$currAnnot[0]);},
 							array_filter($annotEntries,function($item) use($type) { return $item[1] == $type;})),
-					
+
 					";")
 							. "</td>";},$annotTypes));
-			
+
 		}
 		echo "<tr><td>{$row["input"]}</td>{$geneStr}{$annotStr}</tr>";
-	}	
+	}
 	echo "</tbody></table>\n";
-	echo "<script type=\"text/javascript\">\n$(\"#tblResults\").dataTable({dom:'Bfrtip',
-	buttons:[{extend:'csv', title:\"geneList\",fieldSeparator:\"\\t\"},
-	'copy'],bFilter:false,ordering:false,select:\"multi+shiftString\"});\n</script>";
 	// Freeing result and closing connection.
 	pg_free_result($dbRes);
 	pg_close($dbconn);
 }
 ?>
+
+</div>
+</div>
+
+<script type="text/javascript">
+$("#tblResults").dataTable({
+	dom:'Bfrtip',
+	buttons:[{
+		extend:'csv',
+		text:'Download',
+		title:"PpGMLDB_geneList",
+		fieldBoundary: '',
+		fieldSeparator:"\t"
+	},
+	{
+		extend:'excel',
+		text:'Excel',
+		title:"PpGMLDB_geneList",
+		fieldSeparator:"\t"
+	},
+'copy'],
+bFilter:false,
+ordering:false,
+select:"multi+shiftString"
+});
+
+</script>
+
+
 <?php include_once "ppdb_footer.php"; ?>

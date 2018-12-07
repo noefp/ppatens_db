c53152165b069403cdf4dba0c18a3c8a0b8ab8f9
noefp <noeisneo@gmail.com>
2018-12-07 11:27:47 +0100

    file download page

---
+++ downloads.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'ppdb_header.php';
+?>
+
+<div class="page_container">
+
+<br>
+
+<h2 class="text-center yellow_col">Download Files</h2>
+<br>
+
+<?php
+include_once 'download_files.php';
+?>
+
+</div>

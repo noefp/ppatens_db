6b282f7e7ac937dc95a874271f3fdb305f3414bb
noefp <noeisneo@gmail.com>
2018-12-07 15:14:42 +0100

    files to download

--- download_files.php
+++ download_files.php
@@ -7,8 +7,10 @@ function get_dir_and_files($dir_name) {
 
     $pattern='/^\./';
     if (is_dir($dir_name)){
+
       if ($dh = opendir($dir_name)){
         while (($file_name = readdir($dh)) !== false){
+
           $is_not_file = preg_match($pattern, $file_name, $match);
           if (!$is_not_file) {
             // echo $file_name."<br>";
@@ -22,8 +24,9 @@ function get_dir_and_files($dir_name) {
 }
 
 $seq_files_found = get_dir_and_files("../indexed_files/blast_plus_dbs/"); // call the function
+$seq_files_found2 = get_dir_and_files("../downloads/"); // call the function
 
-if ($seq_files_found) {
+if ($seq_files_found || $seq_files_found2) {
   echo "<h3 class=\"yellow_col\">Sequences</h3><ul class=\"well pp_list\">";
 }
 
@@ -32,10 +35,25 @@ foreach ($seq_files_found as $file) {
   $is_fasta = preg_match('/\.fasta$/', $file, $match);
 
   if ($is_fasta) {
-    $file1 = str_replace("_"," ",$file);
 
-      if ($file1) {
-        echo "<li><a href=\"../indexed_files/blast_plus_dbs/$file\" download>$file1</a></li>";
+      if ($file) {
+        echo "<li><a href=\"../indexed_files/blast_plus_dbs/$file\" download>$file</a></li>";
+      } else {
+        echo "<li>No Sequeces Found.</li>";
+      }
+  }
+}
+
+foreach ($seq_files_found2 as $file) {
+
+  $is_fasta = preg_match('/\.fasta$/', $file, $match);
+  $is_zip = preg_match('/\.zip/', $file, $match);
+
+  if ($is_fasta || $is_zip) {
+    // $file1 = str_replace("_"," ",$file);
+
+      if ($file) {
+        echo "<li><a href=\"../downloads/$file\" download>$file</a></li>";
       } else {
         echo "<li>No Sequeces Found.</li>";
       }

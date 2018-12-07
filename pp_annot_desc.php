02e29eaefd38d4be2782c9e0830c1270022e718b
noefp <noeisneo@gmail.com>
2018-12-07 10:48:25 +0100

    new links

--- pp_annot_desc.php
+++ pp_annot_desc.php
@@ -27,32 +27,36 @@ while ($line = pg_fetch_array($sp_res, null, PGSQL_ASSOC)) {
     $sp_id = $line["annot_term"];
     $sp_desc = $line["annot_desc"];
     $sp_link = 'http://www.uniprot.org/uniprot/'.preg_replace('/sp\|(.+)\|.+/','$1',$sp_id);
-    echo "<tr><td><a href=\"$sp_link\" target=\"_blank\">$sp_id</a></td><td>$sp_desc</td><td><a href=\"$sp_link\" target=\"_blank\">SwissProt</a></td></tr>\n";
+    echo "<tr><td><a href=\"$sp_link\" target=\"_blank\">$sp_id</a></td><td>$sp_desc</td><td>SwissProt</td></tr>\n";
 }
 while ($line = pg_fetch_array($at_res, null, PGSQL_ASSOC)) {
     $tair_id = $line["annot_term"];
     $tair_desc = $line["annot_desc"];
     $tair_link = 'http://www.arabidopsis.org/servlets/TairObject?type=locus&name='.preg_replace('/\.\d$/','',$tair_id);
 
-    echo "<tr><td><a href=\"$tair_link\" target=\"_blank\">$tair_id</a></td><td>$tair_desc</td><td><a href=\"$tair_link\" target=\"_blank\">TAIR</a></td></tr>\n";
+    echo "<tr><td><a href=\"$tair_link\" target=\"_blank\">$tair_id</a></td><td>$tair_desc</td><td>TAIR</td></tr>\n";
 }
 while ($line = pg_fetch_array($nr_res, null, PGSQL_ASSOC)) {
     $nr_id = $line["annot_term"];
     $nr_desc = $line["annot_desc"];
     $nr_link = 'https://www.ncbi.nlm.nih.gov/protein/'.preg_replace('/.+\|(.+)\|$/','$1',$nr_id);
 
-    echo "<tr><td><a href=\"$nr_link\" target=\"_blank\">$nr_id</a></td><td>$nr_desc</td><td><a href=\"$nr_link\" target=\"_blank\">NCBI</a></td></tr>\n";
+    echo "<tr><td><a href=\"$nr_link\" target=\"_blank\">$nr_id</a></td><td>$nr_desc</td><td>NCBI</td></tr>\n";
 }
 while ($line = pg_fetch_array($pz_res, null, PGSQL_ASSOC)) {
     $pz_id = $line["annot_term"];
     $pz_desc = $line["annot_desc"];
     $pz_link = 'https://phytozome.jgi.doe.gov/pz/portal.html#!gene?search=1&detail=1&method=5013&searchText=transcriptid:'.$pz_id;
 
-    echo "<tr><td><a href=\"$pz_link\" target=\"_blank\">$gene_name_displayed</a></td><td>$pz_desc</td><td><a href=\"$pz_link\" target=\"_blank\">Phytozome</a></td></tr>\n";
+    echo "<tr><td><a href=\"$pz_link\" target=\"_blank\">$gene_name_displayed</a></td><td>$pz_desc</td><td>Phytozome</td></tr>\n";
 }
+echo "<tr><td><a href=\"https://phytozome.jgi.doe.gov/jbrowse/index.html?data=genomes%2FPpatens&tracks=DNA%2CTranscripts%2CRNAExpression&highlight=&loc=$gene_name_displayed\" target=\"_blank\">$gene_name_displayed</a></td><td>Phytozome Genome browser</td><td>Phytozome</td></tr>\n";
+echo "<tr><td><a href=\"https://genomevolution.org/coge/FeatView.pl?accn=$gene_name_displayed\" target=\"_blank\">$gene_name_displayed</a></td><td>CoGe gene view</td><td>CoGe</td></tr>\n";
 
 $plaza_gene = preg_replace('/V3\.1/','',$gene_name_displayed);
-echo "<tr><td><a href=\"https://bioinformatics.psb.ugent.be/plaza/genes/redirect_gene/3218/$plaza_gene\" target=\"_blank\">$gene_name_displayed</a></td><td></td><td><a href=\"https://bioinformatics.psb.ugent.be/plaza/genes/redirect_gene/3218/$gene_name_displayed\" target=\"_blank\">PLAZA</a></td></tr>\n";
+echo "<tr><td><a href=\"https://bioinformatics.psb.ugent.be/plaza/genes/redirect_gene/3218/$plaza_gene\" target=\"_blank\">$gene_name_displayed</a></td><td>PLAZA gene view</td><td>PLAZA</td></tr>\n";
+echo "<tr><td><a href=\"https://plants.ensembl.org/Physcomitrella_patens/Gene/Summary?g=$gene_name_displayed\" target=\"_blank\">$gene_name_displayed</a></td><td>Ensembl gene view</td><td>Ensembl</td></tr>\n";
+echo "<tr><td><a href=\"http://plants.ensembl.org/Physcomitrella_patens/Location/View?db=core;g=$gene_name_displayed\" target=\"_blank\">$gene_name_displayed</a></td><td>Ensembl Genome browser</td><td>Ensembl</td></tr>\n";
 echo "</table>\n\n";
 
 // Free resultset

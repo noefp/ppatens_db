741f09344ea38cce30c99f077acb99b78ee56e20
noefp <noeisneo@gmail.com>
2019-02-05 10:48:08 +0100

    new header style

--- ppdb_header.php
+++ ppdb_header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>P. patens DB</title>
+    <title>PpGML DB</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="favicon.ico">
@@ -23,49 +23,83 @@
 	<script type="text/javascript" charset="utf8" src="js/DataTables/Select-1.2.6/js/dataTables.select.min.js"></script>
 	  </head>
 
-  <body>
 
-    <!-- <div class="container-fluid imoss-top">
-      <p class="pull-left cover-title">
-        <i>P. patens</i> DB
-      </p>
 
-      <div style="background: url(img/cover.jpg) left center;">
-       <img class="cover-img" src="img/cover.jpg" style="visibility: hidden;" />
-      </div>
-    </div> -->
-<div id="wrapper">
-    <nav class="navbar navbar-default" style="border-radius: 0px;">
-      <div id="moss_toolbar" class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="pp_search_input.php"><i>P. patens</i> gene model lookup</a>
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
+
+    <body>
+
+      <div class="container-fluid imoss-top">
+        <p class="pull-left cover-title">
+          <i>P. patens</i> Gene Model Lookup DB
+        </p>
+
+        <div style="background: url(img/cover.jpg) center center; background-size:cover;">
+         <img class="cover-img" src="img/cover.jpg" style="visibility: hidden;" />
         </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav">
-            <li><a href="blast_input.php">BLAST</a></li>
-            <li><a href="downloads.php">Downloads</a></li>
-            <li><a href="links.php">Links</a></li>
-            <!-- <li class="dropdown"><a id="overview_link" class="menu_click_dropdown dropdown-toggle pointer_cursor" data-toggle="dropdown">Tools</a>
-              <ul class="dropdown-menu">
-                <li><a href="https://peatmoss.online.uni-marburg.de/expression_viewer/input">PEATmoss</a></li>
-                <li><a href="https://plantcode.online.uni-marburg.de/tapscan/index.php">TAPscan</a></li>
-              </ul>
-            </li> -->
-          </ul>
-        </div>
+        <!-- <img class="pull-left cover-img" src="img/cover.jpg"> -->
       </div>
-    </nav>
 
-<script>
+      <div id="wrapper">
 
-  jQuery(document).ready(function() {
-    jQuery(".menu_click_dropdown").removeClass("dropdown-toggle");
-    jQuery(".menu_click_dropdown").removeAttr("data-toggle");
-  });
+      <nav class="navbar navbar-default" data-spy="affix" data-offset-top="277">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="pp_search_input.php"><span class="glyphicon glyphicon-home" style="line-height: 0;"></span></a>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+              <li><a href="blast_input.php">BLAST</a></li>
+              <li><a href="downloads.php">Downloads</a></li>
+              <li><a href="links.php">Links</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-</script>
+
+  <style>
+
+    .affix {
+      top: 0;
+      width: 100%;
+      z-index:100;
+    }
+
+    .affix + .pg_content {
+      padding-top: 72px;
+    }
+
+    .cover-img {
+      height: 277px;
+      overflow: hidden;
+    }
+
+    .cover-title {
+      position: absolute;
+      padding:10px;
+      margin-top:200px;
+      font-size: 24px;
+      color:#fff;
+      width: 50%;
+      background: black; /* For browsers that do not support gradients */
+      background-color: rgba(0, 0, 0, 0.5);
+      background: -webkit-linear-gradient(left, rgba(0, 0, 0, 0.8) , rgba(0, 0, 0, 0)); /* For Safari 5.1 to 6.0 */
+      background: -o-linear-gradient(right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0)); /* For Opera 11.1 to 12.0 */
+      background: -moz-linear-gradient(right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0)); /* For Firefox 3.6 to 15 */
+      background: linear-gradient(to right, rgba(0, 0, 0, 0.8) , rgba(0, 0, 0, 0)); /* Standard syntax */
+    }
+
+    .imoss-top {
+      background-color: #a7d0e5;
+      width: 100%;
+      height: 277px;
+      padding:0px
+    }
+
+
+  </style>

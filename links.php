ef937e9d42361f9055cd64a045438e0d3892ee5b
noefp <noeisneo@gmail.com>
2019-03-06 14:02:41 +0100

    publications in links

--- links.php
+++ links.php
@@ -51,6 +51,43 @@ include_once 'ppdb_header.php';
 
 </ul>
 
+<h2 class="text-center yellow_col">Publications of interest</h2>
+<br>
+<ul class="well pp_list">
+  <li class="pubtext">
+    <span class="small">Perroud P.F., Haas F.B., Hiss M., Ullrich K.K., Alboresi A., Amirebrahimi M., Barry K.,
+      Bassi R., Bonhomme S., Chen H., Coates J., Fujita T., ..., Szövényi P., Sreedasyam A., Grimwood J., Stacey G.,
+      Schmutz J., <b>Rensing S.A.</b></span> (2018)
+    <br>
+    <b>The <i>Physcomitrella patens</i> gene atlas project: large scale RNA-seq based expression data.</b>
+    <br>
+    <a href="http://dx.doi.org/10.1111/tpj.13940" target="_blank">Plant J. 95(1):168</a>
+  </li>
+  <li class="pubtext">
+    <span class="small">Lang, D., Ullrich, K. K., Murat, F., Fuchs, J., Jenkins, J., Haas, F. B., Piednoel, M., Gundlach, H.,
+    Van Bel, M., Meyberg, R., ..., Reski, R., Cuming, A. C., Tuskan, J., Maumus, F., Salse, J., Schmutz, J. and <b>Rensing, S. A.</b></span> (2017)
+    <br>
+    <b>The <i>Physcomitrella patens</i> chromosome‐scale assembly reveals moss genome structure and evolution</b>
+    <br>
+    <a href="http://onlinelibrary.wiley.com/doi/10.1111/tpj.13801/abstract;jsessionid=1A15EE810F653553A3CD113A0D61162C.f02t03" target="_blank">Plant J. 93(3):515</a>
+  </li>
+  <li class="pubtext">
+     <span class="small">Zimmer A.D., Lang D., Buchta K., Rombauts S., Nishiyama T., Hasebe M., Van de Peer Y., <b> Rensing S.A. </b>, Reski R.</span> (2013)
+     <br>
+     <b>Reannotation and extended community resources for the genome of the non-seed plant <em> Physcomitrella patens </em> provide insights into the evolution of plant gene structures and functions</b>.
+     <br>
+     <a href="http://www.biomedcentral.com/1471-2164/14/498" target="_blank">BMC Genomics 14:498</a>
+  </li>
+  <li>
+    <span class="small"><b>Rensing S.A.</b>, Lang D., Zimmer A.D., Terry A.,  Salamov A., Shapiro H., Nishiyama T.,
+      Perroud P.-F., Lindquist E.,  Kamisugi Y., Tanahashi T., Sakakibara K., Fujita T., ..., Reski R., Grigoriev  I.V.,
+      Quatrano R.S., Boore J.L. </span>(2008) <br>
+      <b>The genome of the moss <em>Physcomitrella patens</em> reveals evolutionary insights into the conquest of land by plants.</b>
+      <br>
+    <a href="http://www.sciencemag.org/cgi/content/abstract/1150646" target="_blank">Science 319:64-69</a>
+ </li>
+
+</ul>
 </div>
 
 <?php include_once 'ppdb_footer.php';?>

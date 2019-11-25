73a732121f6a8d95c42657e93c40e3d5a0b2674c
noefp <noeisneo@gmail.com>
2019-11-25 09:43:18 +0100

    searched term well style changed

--- pp_search_output.php
+++ pp_search_output.php
@@ -12,7 +12,7 @@ $dbconn = pg_connect(getConnectionString())
 $search_input = test_input($_GET["search_keywords"]);
 $max_row = 25;
 
-echo "\n<br><h3 class=\"yellow_col\">Gene ID or keyword searched</h3>\n<div class=\"well\">$search_input</div><br>\n";
+echo "\n<br><h3 class=\"yellow_col\">Gene ID or keyword searched</h3>\n<div class=\"well\" style=\"background-color:#060;color:#fff\">$search_input</div><br>\n";
 
 function test_input($data) {
   $data = preg_replace('/[\<\>]+/',' ',$data);

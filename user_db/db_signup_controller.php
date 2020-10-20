379dc1b489d9c3f6093d99a285babf7ae9be2325
noefp <noeisneo@gmail.com>
2020-10-20 13:46:22 +0200

    improved email header

--- user_db/db_signup_controller.php
+++ user_db/db_signup_controller.php
@@ -13,15 +13,19 @@ try {
       $sql2 = "INSERT INTO user_info (first_name,last_name,institution,department,city,country,user_id) VALUES (?,?,?,?,?,?,?)";
       $db->prepare($sql2)->execute([$_POST['first_name'], $_POST['last_name'],$_POST['university'],$_POST['department'],$_POST['city'],$_POST['country'], $userId]);
 
+      $headers = 'From: '.$contact_email."\r\n".
+      'Reply-To:'.$contact_email."\r\n";
 
-      $msg = "Your account in PEATmoss and the <i>P. patens</i> Gene Model Lookup DB was created.<br>We will contact you to confirm your registration. Please contact us if you do not get confirmation in the next 3 days.<br><br>Kind regards,<br>PEATmoss team.";
+      $msg = "Your account in PEATmoss and the PpGML DB was created.\r\n
+      We will contact you to confirm your registration.\r\n
+      Please contact us if you do not get confirmation in the next 3 days.\r\n\r\n
+      Kind regards,\r\n
+      The PEATmoss team.\r\n";
       // use wordwrap() if lines are longer than 70 characters
       $msg = wordwrap($msg,70);
-      mail($_POST['email'],"your account in PEATmoss was created",$msg, $contact_email);
+      mail($_POST['email'],"your account in PEATmoss was created",$msg, $headers);
 
-
-    // echo 'Your user was created. We will validate it in the next working hours. Please contact us if you do not get confirmation in the next 3 days.';
-    echo 'id: '.$userId.' Your user was created. We will contact you to confirm your registration. Please contact us if you do not get confirmation in the next 3 days.';
+    echo 'Your user was created. We will contact you to confirm your registration. Please contact us if you do not get confirmation in the next 3 days.';
 }
 catch (\Delight\Auth\InvalidEmailException $e) {
     echo 'Invalid email address';

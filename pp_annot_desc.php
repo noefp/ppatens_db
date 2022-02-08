fae6370b9619d18d78b412aac87f35048b61f8d4
Noé Fernández Pozo <noeisneo@gmail.com>
2022-02-08 15:41:17 +0100

    Update pp_annot_desc.php
    Removed harcoded link

--- pp_annot_desc.php
+++ pp_annot_desc.php
@@ -60,7 +60,7 @@ echo "<tr><td><a href=\"https://bioinformatics.psb.ugent.be/plaza/genes/redirect
 echo "<tr><td><a href=\"https://plants.ensembl.org/Physcomitrella_patens/Gene/Summary?g=$gene_name_displayed\" target=\"_blank\">$gene_name_displayed</a></td><td>Ensembl gene view</td><td>Ensembl</td></tr>\n";
 echo "<tr><td><a href=\"http://plants.ensembl.org/Physcomitrella_patens/Location/View?db=core;g=$gene_name_displayed\" target=\"_blank\">$gene_name_displayed</a></td><td>Ensembl Genome browser</td><td>Ensembl</td></tr>\n";
 echo "<tr><td><a href=\"https://plantcode.online.uni-marburg.de/tapscan/tap_gene.php?gene_name=$gene_name_displayed\" target=\"_blank\">$gene_name_displayed</a></td><td>Transcription factors and regulators</td><td>TAPscan</td></tr>\n";
-echo "<tr><td><a href=\"https://peatmoss.online.uni-marburg.de/expression_viewer/input?input_gene=$gene_name_displayed\" target=\"_blank\">$gene_name_displayed</a></td><td>Expression data</td><td>PEATmoss</td></tr>\n";
+echo "<tr><td><a href=\"/expression_viewer/input?input_gene=$gene_name_displayed\" target=\"_blank\">$gene_name_displayed</a></td><td>Expression data</td><td>PEATmoss</td></tr>\n";
 echo "</table>\n\n";
 
 // Free resultset
